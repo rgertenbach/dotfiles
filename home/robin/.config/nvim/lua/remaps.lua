dcfe5d514a7d44053c0c87913773114bf660f00e
Robin Gertenbach <RobinGertenbach@gmail.com>
2023-01-01 11:37:11 +0000

    Add keymaps and packer bootstrap

--- home/robin/.config/nvim/lua/remaps.lua
+++ home/robin/.config/nvim/lua/remaps.lua
@@ -2,3 +2,11 @@ vim.g.mapleader = " "
 
 -- Toggle netrw to the left
 vim.keymap.set("n", "<leader>e", vim.cmd.Vex)
+
+-- Use <leader> k/j to move lines up or down.
+vim.keymap.set("n", "<leader>k", ":move .-2<CR>==")
+vim.keymap.set("n", "<leader>j", ":move .+1<CR>==")
+-- c
+-- e
+-- b
+-- a
