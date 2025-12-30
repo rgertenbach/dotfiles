45b630a076b2fdbd9e02d864d45f49bb299f14d4
Robin Gertenbach <rgertenbach@google.com>
2025-12-30 10:46:59 -0800

    Back to configs

--- dot_config/nvim/lua/rgertenbach/plugins/init.lua
+++ dot_config/nvim/lua/rgertenbach/plugins/init.lua
@@ -26,7 +26,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ':TSUpdate',
     config = function()
-      require 'nvim-treesitter.config'.setup {
+      require 'nvim-treesitter.configs'.setup {
         ignore_install = { "help" },
         sync_install = true,
         auto_install = false,
