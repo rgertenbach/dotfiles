dc532aed1bb3561b0003eeac786c97365b008548
Robin Gertenbach <RobinGertenbach@gmail.com>
2026-05-18 20:33:31 -0700

    Make ts_ls run

--- dot_config/nvim/lua/rgertenbach/init.lua
+++ dot_config/nvim/lua/rgertenbach/init.lua
@@ -87,6 +87,21 @@ vim.lsp.config("hls", {
 })
 
 vim.lsp.config("ts_ls", {
+  cmd = function(dispatchers, config)
+    local cmd = 'typescript-language-server'
+    if (config or {}).root_dir then
+      local local_cmd = vim.fs.joinpath(config.root_dir, 'node_modules/.bin', cmd)
+      if vim.fn.executable(local_cmd) == 1 then cmd = local_cmd end
+    end
+    return vim.lsp.rpc.start({ cmd, '--stdio' }, dispatchers)
+  end,
+  root_dir = function(bufnr, on_dir)
+    local root_markers = {
+      { "package-lock.json", "yarn.lock", "pnpm-lock.yaml" }, { ".git" }
+    }
+    on_dir(vim.fs.root(bufnr, root_markers) or vim.fn.getcwd())
+  end,
+  filetypes = { "javascript", "typescript" },
   settings = { implicitProjectConfiguration = { checkJs = true } }
 })
 
@@ -108,4 +123,3 @@ vim.api.nvim_create_autocmd(
     end
   }
 )
-
