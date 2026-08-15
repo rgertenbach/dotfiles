25eab4bac58ee0e627d0ca179ab9959bc3e5c271
Robin Gertenbach <RobinGertenbach@gmail.com>
2026-08-15 12:38:02 -0700

    Fix autocmd removal

--- dot_config/nvim/lsp/hls.lua
+++ dot_config/nvim/lsp/hls.lua
@@ -15,7 +15,7 @@ return {
   on_attach = function(_, bufnr)
     -- Cabal files don't support documentHighlight right now.
     if vim.bo.filetype == "cabal" then
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "LsPDocHighlight" })
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "LspDocHighlight" })
     end
   end
 }
